f9e155daba51cac9cc4dcc31b175dabaaa788c2c
Tomasz Golonko <91921@student.pb.edu.pl>
2026-05-15 23:39:42 +0200

    fix(entities): small fix in some entities

---
+++ src/KuchniaUCygana.Domain/Entities/Logistics/BagMovementLog.cs
@@ -0,0 +1,31 @@
+﻿using KuchniaUCygana.Domain.Common;
+using KuchniaUCygana.Domain.Enums;
+using ServiceStack.AI;
+using ServiceStack.DataAnnotations;
+
+namespace KuchniaUCygana.Domain.Entities.Logistics;
+
+/// <summary>
+/// Rejestrowanie każdego "przekazania" torby 
+/// (np. Magazyn -> Kierowca -> Klient -> Kierowca)
+/// </summary>
+
+public sealed class BagMovementLog : BaseEntity
+{
+    public int ThermalBagId { get; set; }
+
+    public BagStatus FromStatus { get; set; }
+
+    public BagStatus ToStatus { get; set; }
+
+    public int? DriverId {get; set;} 
+
+    public int? RouteStopId {get; set;}
+
+    public void UpdateStatus(BagStatus newStatus)
+    {
+        this.FromStatus = this.ToStatus;
+        this.ToStatus = newStatus;
+    }
+}
+
