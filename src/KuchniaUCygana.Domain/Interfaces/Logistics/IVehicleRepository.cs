5d46c0e34dddeb179d4b6156d0b1ef312086a617
Tomasz Golonko <91921@student.pb.edu.pl>
2026-05-16 14:17:50 +0200

    feat: changed repository interfaces

---
+++ src/KuchniaUCygana.Domain/Interfaces/Logistics/IVehicleRepository.cs
@@ -0,0 +1,13 @@
+using KuchniaUCygana.Domain.Entities.Logistics;
+using KuchniaUCygana.Domain.Interfaces;
+
+namespace KuchniaUCygana.Domain.Interfaces;
+
+/// <summary>
+/// Definicja operacji bazodanowych na pojazdach
+/// </summary>
+
+public interface IVehicleRepository : IRepository<Vehicle>
+{
+    // 
+}
