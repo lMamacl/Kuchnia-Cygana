02a8310752be9c5a1b53d9f7458391a99cb2fc2b
Tomasz Golonko <91921@student.pb.edu.pl>
2026-05-19 14:16:50 +0200

    feat: add logistics repositories

---
+++ src/KuchniaUCygana.Infrastructure/Persistence/Repositories/DriverRepository.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KuchniaUCygana.Domain.Entities.Logistics;
+using KuchniaUCygana.Domain.Interfaces.Logistics;
+using KuchniaUCygana.Infrastructure.Persistence.ConnectionFactory;
+using ServiceStack.OrmLite;
+
+namespace KuchniaUCygana.Infrastructure.Persistence.Repositories;
+
+/// <summary>
+/// Repozytorium kierowców
+/// </summary>
+public sealed class DriverRepository : BaseRepository<Driver>, IDriverRepository
+{
+    public DriverRepository(IDbConnectionFactory connectionFactory)
+        : base(connectionFactory)
+    {
+    }
+
+    // Dodatkowe metody specyficzne dla kierowców, np.:
+    // public async Task<Driver?> GetByPhoneNumberAsync(string phoneNumber)
+    // {
+    //     using var db = Factory.CreateConnection();
+    //     return await db.SingleAsync<Driver>(d => d.PhoneNumber == phoneNumber && !d.IsDeleted);
+    // }
+}
