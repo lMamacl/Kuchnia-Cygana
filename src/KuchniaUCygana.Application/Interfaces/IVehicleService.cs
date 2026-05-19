ffc3522f8387462fcf86223aeb1f512f96c86263
Tomasz Golonko <91921@student.pb.edu.pl>
2026-05-19 23:55:25 +0200

    feat: add vehicle service

---
+++ src/KuchniaUCygana.Application/Interfaces/IVehicleService.cs
@@ -0,0 +1,12 @@
+using KuchniaUCygana.Application.DTOs.Logistics;
+
+namespace KuchniaUCygana.Application.Interfaces;
+
+public interface IVehicleService
+{
+    Task<IEnumerable<VehicleDto>> GetAllAsync();
+    Task<VehicleDto?> GetByIdAsync(int id);
+    Task<VehicleDto> CreateAsync(CreateVehicleRequest request);
+    Task<VehicleDto?> UpdateAsync(int id, UpdateVehicleRequest request);
+    Task<bool> DeleteAsync(int id);
+}
