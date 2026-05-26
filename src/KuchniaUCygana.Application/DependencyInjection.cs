a66a73fa60fc18abe35d52442603ed0f912cc033
Tomasz Golonko <91921@student.pb.edu.pl>
2026-05-26 10:58:50 +0200

    feat: add repositories to dependency injection

--- src/KuchniaUCygana.Application/DependencyInjection.cs
+++ src/KuchniaUCygana.Application/DependencyInjection.cs
@@ -14,6 +14,7 @@ public static class DependencyInjection
         services.AddValidatorsFromAssemblyContaining(typeof(DependencyInjection));
         
         services.AddScoped<IUserService, UserService>();
+        services.AddScoped<Services.Logistics.GeocodingOrchestrator>();
         return services;
     }
 }
