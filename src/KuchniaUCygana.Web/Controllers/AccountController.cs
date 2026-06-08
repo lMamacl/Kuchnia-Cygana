2fa96d37a29c71fc12d3e955e99982bea65f1534
Daav337 <dawidjanczylogitlab@gmail.com>
2026-06-08 20:40:03 +0200

    Drobne poprawki głównych widoków: - Dodanie opcji przejscia do panelu zarzadzania adresami na gornym pasku glownego menu - Teraz poprawnie po zalogowaniu przekierowuje klienta na glowna strone, bo byl blad ze trafia na pusty widok panelu zarzadzania

--- src/KuchniaUCygana.Web/Controllers/AccountController.cs
+++ src/KuchniaUCygana.Web/Controllers/AccountController.cs
@@ -73,9 +73,7 @@ public sealed class AccountController : Controller
     [HttpGet]
     public IActionResult Index()
     {
-        ViewData["Title"] = "Konto";
-        ViewData["Description"] = "Centrum konta uzytkownika.";
-        return View();
+        return RedirectToRoleHome(GetPrimaryRole(User), returnUrl: null);
     }
 
     [Authorize]
@@ -346,7 +344,7 @@ public sealed class AccountController : Controller
             UserRoles.Dietitian => RedirectToAction("Index", "DietEditor"),
             UserRoles.Logistics or UserRoles.LogisticsManager => RedirectToAction("Index", "Logistics"),
             UserRoles.Driver or UserRoles.DriverManager => RedirectToAction("Index", "DriverMobile"),
-            UserRoles.Client => RedirectToAction("Index", "Account"),
+            UserRoles.Client => RedirectToAction("Index", "Home"),
             _ => RedirectToAction("Index", "Home"),
         };
     }
